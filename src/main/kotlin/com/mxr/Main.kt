8f30f5fe5779b84ff3afcb3a11f12c60c7d06291
mixer <ferenc.tollas@gmail.com>
2019-03-20 09:20:34 +0100

    Main.kt: Bug fixes

--- src/main/kotlin/com/mxr/Main.kt
+++ src/main/kotlin/com/mxr/Main.kt
@@ -1,50 +1,44 @@
 @file:JvmName("JSONParserExample")
-
 package com.mxr
 
 import java.io.File
-import kotlin.system.measureTimeMillis
+import java.lang.reflect.AnnotatedArrayType
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.system.exitProcess
 
 
 fun main(str: Array<String>) {
-    val jsonParser = JSONParser()
+
     val fileName = "test2.json"
 
     if (!File(fileName).exists()) {
-        throw Exception ("JSON file does not exists!!")
+        print("JSON file does not exists!!")
+        exitProcess(-1)
     }
 
     val content = File(fileName).readText()
-    
-    
-    lateinit var result: Any
-    val time = measureTimeMillis {
-         result = jsonParser.parse(content)
-    }
+
+    val jsonParser = JSONParser()
+    var result = jsonParser.parse(content)
 
     when(result) {
-        is ArrayList<*> -> produceArrayResult(jsonParser, result as ArrayList<*>)
         is Map<*,*> -> produceMapResult(jsonParser, (result as Map<String, Any>))
+        is Array<*> -> produceArrayResult(jsonParser, result as ArrayList<*>)
     }
-
-    println("\nJSON parse time: $time milliseconds")
 }
 
 private fun produceArrayResult(parser: JSONParser, result: ArrayList<*>) {
             val size = result.size
             println("Result is array and size: $size")
             println(result.first().javaClass.getName())
-            val picture = parser.getObject("picture", result.first()  as Map<String, Any>)
-            if (picture is String)
-                println("\nPicture: $picture")
+            val picture = parser.getObject("tags", result.first() as Map<String, Any>, String)
+            println("\nPicture: $picture")
 }
 
 private fun produceMapResult(parser: JSONParser, result: Map<String, Any>) {
-            val size = result.size
-            println("Result is map and size: $size")
-            var name = parser.getObject("object1/name", result)
-            println("Name: $name")
-            name = parser.getObject("object4/object5/name", result)
-            println("Name: $name")
-    
+    val size = result.size
+    println("Result is map and size: $size")
+    val variable = parser.getObject("tags", result, HashMap<String, String>())
+    println("Name: $variable")
 }
